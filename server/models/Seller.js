eb2079f11948918adebf992feecb5eae05692576
thatanjan <anjan.cules@gmail.com>
2021-05-08 12:40:03 +0600

    Seller model will store the data about user if he is a seller

---
+++ server/models/Seller.js
@@ -0,0 +1,25 @@
+import { Schema, model } from 'mongoose'
+import Product from './Product'
+
+const objectId = Schema.Types.ObjectId
+
+const schema = new Schema({
+	user: {
+		type: objectId,
+		required: true,
+		ref: 'user',
+	},
+	company: {
+		type: String,
+		required: true,
+	},
+	star: {
+		type: Number,
+		default: 0,
+	},
+	products: [{ type: objectId, ref: Product, unique: true }],
+})
+
+const Seller = model('seller', schema)
+
+export default Seller
