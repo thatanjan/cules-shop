fa042d710624bb25ddda65b4d399ed79ae436a0a
thatanjan <anjan.cules@gmail.com>
2021-05-08 12:27:39 +0600

    application is now connected to mongodb

--- server/src/index.js
+++ server/src/index.js
@@ -11,6 +11,21 @@ const app = express()
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
+mongoose
+	.connect(process.env.USERS_DB_URI, {
+		useNewUrlParser: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		console.log('mongodb connected')
+	})
+	.catch(error => {
+		// eslint-disable-next-line
+		console.log(error)
+	})
+
 app.use(cors())
 
 const typeDefs = gql`
