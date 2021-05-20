89c627ce2bc177c55f167df224b314b747b66b5a
thatanjan <anjan.cules@gmail.com>
2021-05-20 09:02:15 +0600

    Prices might get change of a product. So totalprice is unpredictable. That's why totalprice is removed from the Cart model

--- server/src/models/Cart.js
+++ server/src/models/Cart.js
@@ -12,19 +12,11 @@ const product = {
 		type: Number,
 		required: true,
 	},
-	price: {
-		type: Number,
-		required: true,
-	},
 }
 
 const schema = new Schema({
 	products: [product],
 	totalQuantity: {
-		type: Number,
-		default: 0,
-	},
-	totalPrice: {
 		type: Number,
 		default: 0,
 	},
