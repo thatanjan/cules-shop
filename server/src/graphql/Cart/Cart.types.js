18fe4c6455feebb72ee7fc67f56a8ccd451c31af
thatanjan <anjan.cules@gmail.com>
2021-05-21 12:08:26 +0600

    modifyQuantity mutation will increase or decrease the quantity of product in the cart

--- server/src/graphql/Cart/Cart.types.js
+++ server/src/graphql/Cart/Cart.types.js
@@ -3,11 +3,18 @@ import { gql } from 'apollo-server-express'
 const typeDefs = gql`
 	extend type Mutation {
 		addProductToCart(Input: AddProductToCartInput!): SuccessResponse!
+		modifyQuantity(Input: ModifyQuantityInput!): SuccessResponse!
 	}
 
 	input AddProductToCartInput {
 		productID: ID!
 		quantity: Int!
+	}
+
+	input ModifyQuantityInput {
+		productID: ID!
+		type: String!
+		amount: Int!
 	}
 `
 
