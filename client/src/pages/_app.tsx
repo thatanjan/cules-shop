196f0113d1cfbb440d3fbf89206d0c0591f91b3c
thatanjan <anjan.cules@gmail.com>
2021-05-24 18:09:53 +0600

    css is reset with material-ui css baseline

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,5 +1,31 @@
-const MyApp = ({ Component, pageProps }) => {
-	return <Component {...pageProps} />
-}
+import type { AppProps } from 'next/app'
+import React from 'react'
+import Head from 'next/head'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
-export default MyApp
+export default function MyApp(props: AppProps) {
+	const { Component, pageProps } = props
+
+	React.useEffect(() => {
+		// Remove the server-side injected CSS.
+		const jssStyles: any = document.querySelector('#jss-server-side')
+		if (jssStyles) {
+			jssStyles.parentElement.removeChild(jssStyles)
+		}
+	}, [])
+
+	return (
+		<>
+			<Head>
+				<title>Cules Shop</title>
+				<meta
+					name='viewport'
+					content='minimum-scale=1, initial-scale=1, width=device-width'
+				/>
+			</Head>
+
+			<CssBaseline />
+			<Component {...pageProps} />
+		</>
+	)
+}
