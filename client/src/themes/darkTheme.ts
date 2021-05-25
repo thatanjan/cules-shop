195054ee5c0a943510e3a89673903d1614dae6b3
thatanjan <anjan.cules@gmail.com>
2021-05-25 12:58:35 +0600

    Pallete colors are overide to give brand feel. also paper background color is changed to make background more dark.

--- client/src/themes/darkTheme.ts
+++ client/src/themes/darkTheme.ts
@@ -3,6 +3,19 @@ import { createTheme } from '@material-ui/core/styles'
 const theme = createTheme({
 	palette: {
 		mode: 'dark',
+		primary: {
+			light: '#df6843',
+			main: '#ff3c00',
+			dark: '#972e0e',
+		},
+		secondary: {
+			light: '#015f92',
+			main: '#0288d1',
+			dark: '#349fda',
+		},
+		background: {
+			paper: '#121212',
+		},
 	},
 })
 
