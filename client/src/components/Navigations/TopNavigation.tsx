e1b9635a768f7bd98a5a893022068465ab5ef973
thatanjan <anjan.cules@gmail.com>
2021-05-25 09:31:52 +0600

    App title is now styled in the app bar. it is using nextjs link to redirect

--- client/src/components/Navigations/TopNavigation.tsx
+++ client/src/components/Navigations/TopNavigation.tsx
@@ -4,6 +4,10 @@ import Box from '@material-ui/core/Box'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
+import Typography from '@material-ui/core/Typography'
+
+import { APP_TITLE } from 'variables/global'
+import MuiLink from 'components/Links/MuiLink'
 
 interface Props {}
 
@@ -11,7 +15,7 @@ const TopNavigation = (props: Props) => {
 	return (
 		<>
 			<Box sx={{ flexGrow: 1 }}>
-				<AppBar position='static' >
+				<AppBar position='static'>
 					<Toolbar>
 						<IconButton
 							edge='start'
@@ -21,6 +25,18 @@ const TopNavigation = (props: Props) => {
 						>
 							<MenuIcon />
 						</IconButton>
+
+						<Box sx={{ flexGrow: 1 }}>
+							<MuiLink
+								MuiComponent={Typography}
+								href='/'
+								sx={{ display: 'inline' }}
+								component='h1'
+								variant='h5'
+							>
+								{APP_TITLE}
+							</MuiLink>
+						</Box>
 					</Toolbar>
 				</AppBar>
 			</Box>
