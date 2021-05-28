2b8f8c99d53a71aa716790f408db93bb0ecf0f4d
thatanjan <anjan.cules@gmail.com>
2021-05-28 08:16:45 +0600

    A ProductBanner is created for showing people to buy product. It will be a slide show.

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import BannerSlideShow from 'components/Banner/BannerSlideShow'
+import ProductBanner from 'components/Banner/ProductBanner'
 
 const Index = () => {
 	return (
 		<>
 			<BannerSlideShow />
+			<ProductBanner />
 		</>
 	)
 }
