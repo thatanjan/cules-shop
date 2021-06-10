ae445bcb04d3d3c7f1e7eb6fcd151659720f6470
thatanjan <anjan.cules@gmail.com>
2021-06-10 19:40:24 +0600

    Product slide show is created

---
+++ client/src/components/Products/ProductPreviewSlideShow.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { nanoid } from 'nanoid'
+import { makeStyles } from '@material-ui/core/styles'
+
+import ProductPreview from './ProductPreview'
+
+export const useStyles = makeStyles({
+	swiperContainer: {
+		paddingBottom: '1.5rem',
+		'& .swiper-pagination-bullet': {
+			background: 'white',
+		},
+	},
+})
+
+const BannerSlideShow = () => {
+	const { swiperContainer } = useStyles()
+	return (
+		<>
+			<Swiper
+				slidesPerView={1}
+				centeredSlides={false}
+				slidesPerGroupSkip={1}
+				grabCursor
+				keyboard={{
+					enabled: true,
+				}}
+				breakpoints={{
+					'400': {
+						slidesPerView: 2,
+						slidesPerGroup: 2,
+					},
+					'600': {
+						slidesPerView: 3,
+						slidesPerGroup: 3,
+					},
+				}}
+				pagination={{
+					clickable: true,
+				}}
+				loop
+				navigation
+				className={swiperContainer}
+			>
+				{[1, 2, 3, 4, 5].map(() => (
+					<SwiperSlide key={nanoid()}>
+						<ProductPreview />
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</>
+	)
+}
+
+export default BannerSlideShow
