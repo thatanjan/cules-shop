72bd0c77e5491891e58eb3f84882a6a4d190fbbb
thatanjan <anjan.cules@gmail.com>
2021-06-20 15:30:04 +0600

    single category tab and Home accordion added to the home page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -11,6 +11,8 @@ import BannerSlideShow from 'components/Banner/BannerSlideShow'
 import ProductBannerSlideShow from 'components/Banner/ProductBannerSlideShow'
 import ProductSlideShow from 'components/Products/ProductPreviewSlideShow'
 import ProductPreviewTabs from 'components/Tabs/ProductPreviewTabs'
+import SingleCategoryTab from 'components/Tabs/SingleCategoryTab'
+import HomeAccordion from 'components/Accordions/HomePageAccordions/HomeAccordion'
 
 import 'swiper/swiper.min.css'
 import 'swiper/components/pagination/pagination.min.css'
@@ -26,6 +28,8 @@ const Index = () => {
 			<ProductBannerSlideShow />
 			<ProductSlideShow />
 			<ProductPreviewTabs />
+			<SingleCategoryTab tabName='Television' Content={<ProductSlideShow />} />
+			<HomeAccordion />
 		</>
 	)
 }
