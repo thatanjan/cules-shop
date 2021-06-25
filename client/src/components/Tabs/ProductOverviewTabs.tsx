917e7b2dfc28c4c62900926629a5dd874a5a2421
thatanjan <anjan.cules@gmail.com>
2021-06-25 12:50:26 +0600

    Tabs are created for product overview pages

---
+++ client/src/components/Tabs/ProductOverviewTabs.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Box from '@material-ui/core/Box'
+import { nanoid } from 'nanoid'
+import { useTheme } from '@material-ui/core/styles'
+
+interface TabPanelProps {
+	children?: React.ReactNode
+	index: number
+	value: number
+}
+
+function TabPanel(props: TabPanelProps) {
+	const { children, value, index, ...other } = props
+
+	return (
+		<div
+			role='tabpanel'
+			hidden={value !== index}
+			id={`simple-tabpanel-${index}`}
+			aria-labelledby={`simple-tab-${index}`}
+			{...other}
+		>
+			{value === index && children}
+		</div>
+	)
+}
+
+function a11yProps(index: number) {
+	return {
+		id: `simple-tab-${index}`,
+		'aria-controls': `simple-tabpanel-${index}`,
+	}
+}
+
+const tabNames = ['Accessories', 'Description', 'specification', 'reviews']
+
+const ProductOverviewTabs = () => {
+	const [value, setValue] = React.useState(0)
+	const theme = useTheme()
+
+	const handleChange = (_: React.SyntheticEvent, newValue: number) => {
+		setValue(newValue)
+	}
+
+	return (
+		<Box sx={{ width: '100%' }}>
+			<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+				<Tabs value={value} onChange={handleChange} indicatorColor='primary'>
+					{tabNames.map((tab, index) => (
+						<Tab
+							key={nanoid()}
+							sx={{ textTransform: 'capitalize' }}
+							label={tab}
+							{...a11yProps(index)}
+						/>
+					))}
+				</Tabs>
+			</Box>
+
+			{tabNames.map((_, index) => (
+				<TabPanel {...{ value, index, key: nanoid() }}>
+					<Box
+						sx={{
+							border: `1px solid ${theme.palette.primary.main}`,
+							borderRadius: '0.5rem',
+							padding: '1rem',
+						}}
+					>
+						This will have tabs
+					</Box>
+				</TabPanel>
+			))}
+		</Box>
+	)
+}
+
+export default ProductOverviewTabs
--- client/src/pages/product/[productID].tsx
+++ client/src/pages/product/[productID].tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+
+import ProductOverviewTabs from 'components/Tabs/ProductOverviewTabs'
 import ProductOverview from 'components/Products/ProductOverview'
 
 const Product = () => {
 	return (
 		<>
-			{' '}
-			<ProductOverview />{' '}
+			<ProductOverview />
+
+			<ProductOverviewTabs />
 		</>
 	)
 }
