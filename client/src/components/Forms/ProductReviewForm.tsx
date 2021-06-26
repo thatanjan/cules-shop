3758877de4516b36983ef9ee2c10a91f18f885e3
thatanjan <anjan.cules@gmail.com>
2021-06-26 17:28:43 +0600

    Form button and event handler added

--- client/src/components/Forms/ProductReviewForm.tsx
+++ client/src/components/Forms/ProductReviewForm.tsx
@@ -1,11 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, FormEventHandler } from 'react'
 import Box from '@material-ui/core/Box'
 import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 const ProductReviewForm = () => {
 	const [description, setDescription] = useState('')
+
+	const handleSubmit: FormEventHandler<HTMLFormElement> = event => {
+		setDescription('')
+		event.preventDefault()
+	}
+
 	return (
-		<Box component='form'>
+		<Box component='form' onSubmit={handleSubmit}>
 			<TextField
 				label='Description'
 				multiline
@@ -15,6 +22,10 @@ const ProductReviewForm = () => {
 				fullWidth
 				onChange={e => setDescription(e.target.value)}
 			/>
+
+			<Button type='submit' disabled={!description} variant='contained'>
+				Submit
+			</Button>
 		</Box>
 	)
 }
