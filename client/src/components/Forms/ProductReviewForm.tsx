134cf8fb4103e6cd9be827df6f707d4ad23e781d
thatanjan <anjan.cules@gmail.com>
2021-06-26 17:32:48 +0600

    rating component added to the form

--- client/src/components/Forms/ProductReviewForm.tsx
+++ client/src/components/Forms/ProductReviewForm.tsx
@@ -3,16 +3,27 @@ import Box from '@material-ui/core/Box'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 
+import CustomRating from 'components/Ratings/CustomRating'
+
 const ProductReviewForm = () => {
 	const [description, setDescription] = useState('')
+	const [ratingValue, setRatingValue] = useState(0)
 
 	const handleSubmit: FormEventHandler<HTMLFormElement> = event => {
 		setDescription('')
+		setRatingValue(0)
 		event.preventDefault()
 	}
 
 	return (
 		<Box component='form' onSubmit={handleSubmit}>
+			<CustomRating
+				value={ratingValue}
+				onChange={(_, newValue) => {
+					setRatingValue(newValue)
+				}}
+			/>
+
 			<TextField
 				label='Description'
 				multiline
