885089935d6f8f37a5188b727d395c71e2a0cb7f
thatanjan <anjan.cules@gmail.com>
2021-06-27 08:50:59 +0600

    related products tab added

--- client/src/pages/product/[productID].tsx
+++ client/src/pages/product/[productID].tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import SwiperCore, {
+	Keyboard,
+	Scrollbar,
+	Autoplay,
+	Pagination,
+	Navigation,
+} from 'swiper/core'
 
 import ProductOverviewTabs from 'components/Tabs/ProductOverviewTabs'
 import ProductOverview from 'components/Products/ProductOverview'
+import ProductPreviewTabs from 'components/Tabs/ProductPreviewTabs'
+
+import 'swiper/swiper.min.css'
+import 'swiper/components/pagination/pagination.min.css'
+import 'swiper/components/navigation/navigation.min.css'
+import 'swiper/components/scrollbar/scrollbar.min.css'
+
+SwiperCore.use([Keyboard, Scrollbar, Autoplay, Pagination, Navigation])
 
 const Product = () => {
 	return (
@@ -9,6 +24,8 @@ const Product = () => {
 			<ProductOverview />
 
 			<ProductOverviewTabs />
+
+			<ProductPreviewTabs tabNames={['related products']} />
 		</>
 	)
 }
