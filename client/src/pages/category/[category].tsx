0c66eddaa253874331578a77f74f4a9cfe46182a
thatanjan <anjan.cules@gmail.com>
2021-06-27 09:24:07 +0600

    Header added for categroy page

--- client/src/pages/category/[category].tsx
+++ client/src/pages/category/[category].tsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { GetServerSideProps } from 'next'
 import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
 
 interface Props {
 	category: string
 }
 
+const Header = ({ category }: Props) => {
+	return (
+		<Grid container justifyContent='space-between' alignItems='center'>
+			<Grid item component={Typography} variant='h3'>
+				{category}
+			</Grid>
+			<Grid item component={Typography}>
+				showing 1-30 of 40 results
+			</Grid>
+		</Grid>
+	)
+}
+
 const Category = ({ category }: Props) => {
 	return (
 		<>
-			<Typography variant='h3'>{category}</Typography>
+			<Header {...{ category }} />
 		</>
 	)
 }
