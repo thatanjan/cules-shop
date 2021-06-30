f35f8409754c273fd36209f65ba9ea9772bce53e
thatanjan <anjan.cules@gmail.com>
2021-06-30 13:49:44 +0600

    Delete button will be available for Cart page on each product.

--- client/src/components/Products/ProductPreview.tsx
+++ client/src/components/Products/ProductPreview.tsx
@@ -11,6 +11,7 @@ import IconButton from '@material-ui/core/IconButton'
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
 
 const ProductQuantity = dynamic(() => import('./ProductQuantity'))
+const DeleteFromCart = dynamic(() => import('components/Cart/DeleteFromCart'))
 
 export interface Props {
 	twoColumn?: boolean
@@ -67,6 +68,12 @@ const ProductPreview = ({ twoColumn, cartPage }: Props) => {
 							$100
 						</Typography>
 					</Grid>
+
+					{cartPage && (
+						<Grid item>
+							<DeleteFromCart />
+						</Grid>
+					)}
 
 					<Grid item>
 						{cartPage ? (
