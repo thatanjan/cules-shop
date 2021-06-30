3d994c7599bd82ff1a479f1527b440f3ede84baa
thatanjan <anjan.cules@gmail.com>
2021-06-30 15:05:50 +0600

    component for account data is created where personal data will be shown

---
+++ client/src/components/Account/AccountData.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import { nanoid } from 'nanoid'
+
+const AccountData = () => {
+	return (
+		<Grid justifyContent='center' container sx={{ maxWidth: '50rem' }}>
+			{Array(10)
+				.fill(0)
+				.map(() => (
+					<Grid container item sx={{ marginBottom: '.5rem' }} key={nanoid()}>
+						<Grid item xs={3}>
+							Name
+						</Grid>
+						<Grid item xs={2} sm={1}>
+							:
+						</Grid>
+						<Grid item xs={6}>
+							Anjan Shomodder
+						</Grid>
+					</Grid>
+				))}
+		</Grid>
+	)
+}
+
+export default AccountData
