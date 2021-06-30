d08faa7e239b8014c55f25b2df3535f08094daa3
thatanjan <anjan.cules@gmail.com>
2021-06-30 08:56:46 +0600

    A cart page is created

---
+++ client/src/pages/cart/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+interface Props {}
+
+const CartPage = (props: Props) => {
+	return (
+		<div>
+			<Typography component='h1' variant='h3' align='center'>
+				Cart
+			</Typography>
+		</div>
+	)
+}
+
+export default CartPage
