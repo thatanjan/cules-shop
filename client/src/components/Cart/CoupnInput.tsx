693f704c7073fc84304e8cb6c78fc01642e015b8
thatanjan <anjan.cules@gmail.com>
2021-07-03 06:05:20 +0600

    coupn textfield and button will  be aligned to center.

--- client/src/components/Cart/CoupnInput.tsx
+++ client/src/components/Cart/CoupnInput.tsx
@@ -1,14 +1,27 @@
 import React, { useState } from 'react'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
 
 interface Props {}
+
+const useStyles = makeStyles({
+	formContainer: {
+		display: 'grid',
+		gridTemplateColumns: '2fr 1fr',
+		alignItems: 'center',
+		gridColumnGap: '1rem',
+	},
+})
 
 const CoupnInput = (props: Props) => {
 	const [code, setCode] = useState('')
 
+	const { formContainer } = useStyles()
+
 	return (
 		<form
+			className={formContainer}
 			onSubmit={event => {
 				event.preventDefault()
 				setCode('')
@@ -16,7 +29,7 @@ const CoupnInput = (props: Props) => {
 		>
 			<TextField
 				label='Coupn Code'
-				variant='standard'
+				variant='filled'
 				value={code}
 				onChange={event => setCode(event.target.value)}
 				sx={{ marginBottom: '1rem', display: 'flex' }}
