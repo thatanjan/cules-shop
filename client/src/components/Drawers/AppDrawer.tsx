cd423a7515d5c7079a2afacd67552b53c9d5b97e
thatanjan <anjan.cules@gmail.com>
2021-07-05 09:26:24 +0600

    onClick of any drawer item will open up all the submenu. need to fix

--- client/src/components/Drawers/AppDrawer.tsx
+++ client/src/components/Drawers/AppDrawer.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState, Fragment } from 'react'
 import Box from '@material-ui/core/Box'
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
+import Collapse from '@material-ui/core/Collapse'
+
+import ExpandLess from '@material-ui/icons/ExpandLess'
+import ExpandMore from '@material-ui/icons/ExpandMore'
 
 import { nanoid } from 'nanoid'
 
@@ -18,15 +22,35 @@ const data = {
 }
 
 const DrawerList = () => {
+	const [openNest, setOpenNest] = useState(false)
 	return (
 		<Box>
 			<List>
 				{Array(10)
 					.fill(data)
-					.map(({ name }) => (
-						<ListItem button key={nanoid()}>
-							<ListItemText primary={name} />
-						</ListItem>
+					.map(({ name, subCategories }) => (
+						<Fragment key={nanoid()}>
+							<ListItem button>
+								<ListItemText
+									primary={name}
+									onClick={() => setOpenNest(prev => !prev)}
+								/>
+								{openNest ? <ExpandLess /> : <ExpandMore />}
+							</ListItem>
+
+							{openNest && (
+								<Collapse in={openNest} timeout='auto' unmountOnExit>
+									{subCategories &&
+										subCategories.map(item => (
+											<List component='div' disablePadding>
+												<ListItem button>
+													<ListItemText primary={item} />
+												</ListItem>
+											</List>
+										))}
+								</Collapse>
+							)}
+						</Fragment>
 					))}
 			</List>
 		</Box>
