cc11aecc84530e1a668664cde8cbcd54b1faf59e
thatanjan <anjan.cules@gmail.com>
2021-07-08 17:02:32 +0600

    Page title added to the page. not the browser page title

--- client/src/pages/checkout/index.tsx
+++ client/src/pages/checkout/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
 
 import CheckoutForm from 'components/Forms/CheckoutForm'
 
@@ -8,6 +9,10 @@ interface Props {}
 const CheckoutPage = (props: Props) => {
 	return (
 		<>
+			<Typography align='center' variant='h3' component='h1'>
+				Checkout
+			</Typography>
+
 			<Grid container>
 				<Grid item xs={12}>
 					<CheckoutForm />
