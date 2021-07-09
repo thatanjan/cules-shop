04923a5c3e6b614ae6aaa8b3e2b846cc5f1f6bed
thatanjan <anjan.cules@gmail.com>
2021-07-09 15:48:35 +0600

    setShippingAddress reducer created to store shipping address

--- client/src/redux/slices/checkoutSlices.ts
+++ client/src/redux/slices/checkoutSlices.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 import { Values as ShippingValues } from 'components/Forms/BillingForms/ShippingForm'
 
@@ -23,9 +23,13 @@ const initialState: InitialState = {
 export const drawerSlice = createSlice({
 	name: 'drawer',
 	initialState,
-	reducers: {},
+	reducers: {
+		setShippingAddress: (state, { payload }: PayloadAction<ShippingValues>) => {
+			state.shippingValues = payload
+		},
+	},
 })
 
-export const {} = drawerSlice.actions
+export const { setShippingAddress } = drawerSlice.actions
 
 export default drawerSlice.reducer
