26d91b338e85590fb907a2beb9cb68f595382fcb
thatanjan <anjan.cules@gmail.com>
2021-07-09 19:40:34 +0600

    useToggleShowDifferentAddressForm is created. for useClearShippingAddress invalid hook call has been fixed

--- client/src/redux/hooks/useCheckoutHooks.ts
+++ client/src/redux/hooks/useCheckoutHooks.ts
@@ -2,6 +2,7 @@ import { useAppDispatch } from 'redux/hooks/appHooks'
 import {
 	setShippingAddress,
 	clearShippingAddress,
+	toggleShowDifferentAddressForm,
 } from 'redux/slices/checkoutSlices'
 import { Values } from 'components/Forms/BillingForms/ShippingForm'
 
@@ -11,5 +12,12 @@ export const useSetShippingAddress = () => {
 	return (values: Values) => dispatch(setShippingAddress(values))
 }
 
-export const useClearShippingAddress = () => () =>
-	useAppDispatch()(clearShippingAddress())
+export const useToggleShowDifferentAddressForm = () => {
+	const dispatch = useAppDispatch()
+	return () => dispatch(toggleShowDifferentAddressForm())
+}
+
+export const useClearShippingAddress = () => {
+	const dispatch = useAppDispatch()
+	return () => dispatch(clearShippingAddress())
+}
