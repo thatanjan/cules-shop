3c807151aca76d49b7569588058c7f01d3bcacbe
thatanjan <anjan.cules@gmail.com>
2021-07-09 19:26:26 +0600

    shippingForm will be shown if showDifferentAddressForm state is true

--- client/src/components/Forms/BillingForms/ShippingFormContainer.tsx
+++ client/src/components/Forms/BillingForms/ShippingFormContainer.tsx
@@ -3,11 +3,16 @@ import Divider from '@material-ui/core/Divider'
 import Typography from '@material-ui/core/Typography'
 import dynamic from 'next/dynamic'
 
+import { useAppSelector } from 'redux/hooks/appHooks'
+
 const ShippingForm = dynamic(() => import('./ShippingForm'))
 
 interface Props {}
 
 const ShippingFormContainer = (props: Props) => {
+	const showDifferentAddressForm = useAppSelector(
+		state => state.checkout.showDifferentAddressForm
+	)
 	return (
 		<>
 			<Typography variant='h4' component='h2' sx={{ margin: '1rem 0' }}>
@@ -15,7 +20,7 @@ const ShippingFormContainer = (props: Props) => {
 			</Typography>
 			<Divider sx={{ marginBottom: '1rem' }} />
 
-			<ShippingForm />
+			{showDifferentAddressForm && <ShippingForm />}
 		</>
 	)
 }
