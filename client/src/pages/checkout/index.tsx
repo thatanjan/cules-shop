aeda2ebc2c834d275e60218c12b35afe433f50d2
thatanjan <anjan.cules@gmail.com>
2021-07-09 11:53:54 +0600

    shipping address added to checkout page

--- client/src/pages/checkout/index.tsx
+++ client/src/pages/checkout/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
 
 import CheckoutForm from 'components/Forms/CheckoutForm'
 import ShippingFormContainer from 'components/Forms/BillingForms/ShippingFormContainer'
@@ -15,6 +16,29 @@ const CheckoutPage = (props: Props) => {
 			</Typography>
 
 			<Grid container>
+				<Grid item xs={12} sx={{ margin: '1rem 0' }}>
+					<Typography variant='h3' color='secondary' sx={{ marginBottom: '1rem' }}>
+						Shipping Address
+					</Typography>
+					<Divider />
+
+					<Grid container sx={{ margin: '1rem 0' }}>
+						<Grid item xs={12} sx={{ marginBottom: '1rem' }}>
+							<Typography variant='h5'>Current Address</Typography>
+						</Grid>
+
+						<Grid item xs={4}>
+							name
+						</Grid>
+						<Grid item xs={2}>
+							:
+						</Grid>
+						<Grid item xs={6}>
+							Anjan Shomodder
+						</Grid>
+					</Grid>
+				</Grid>
+
 				<Grid item xs={12}>
 					<ShippingFormContainer />
 				</Grid>
