45f3c64ac6e522f408e4cf4c04efdce10eea31b2
thatanjan <anjan.cules@gmail.com>
2021-07-11 21:12:20 +0600

    logout page will delete jwt cookie and redirect to home page

--- client/src/pages/authentication/logout.tsx
+++ client/src/pages/authentication/logout.tsx
@@ -1,3 +1,9 @@
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import cookie from 'js-cookie'
+
+import { TOKEN_NAME } from 'variables/global'
+
 import { resetState as resetCheckoutState } from 'redux/slices/checkoutSlices'
 import { logout } from 'redux/slices/userSlice'
 import { resetState as resetDrawerState } from 'redux/slices/drawerSlice'
@@ -8,9 +14,19 @@ interface Props {}
 const Logout = (props: Props) => {
 	const dispatch = useAppDispatch()
 
+	const { push } = useRouter()
+
 	dispatch(logout())
 	dispatch(resetCheckoutState())
 	dispatch(resetDrawerState())
+
+	cookie.remove(TOKEN_NAME)
+
+	useEffect(() => {
+		setTimeout(() => {
+			push('/')
+		}, 3000)
+	})
 
 	return null
 }
