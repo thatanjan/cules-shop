c8b4a8ad6a55fa8046a3a5bc19081174bc73b066
thatanjan <anjan.cules@gmail.com>
2021-07-12 14:44:56 +0600

    useStoreID hook will store all id to user state on redux

---
+++ client/src/redux/hooks/useUserHooks.ts
@@ -0,0 +1,10 @@
+import { login, Ids, logout } from 'redux/slices/userSlice'
+import { useAppDispatch } from 'redux/hooks/appHooks'
+
+export const useStoreID = (ids: Ids) => {
+	const dispatch = useAppDispatch()
+
+	if (!ids.userID) return dispatch(logout())
+
+	dispatch(login(ids))
+}
