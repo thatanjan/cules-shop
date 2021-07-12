8484fb7cd04ec7945f9555986b9c2b4fb0753f9b
thatanjan <anjan.cules@gmail.com>
2021-07-12 14:30:23 +0600

    user slice will have sellerID as a state. Also in the payload for login

--- client/src/redux/slices/userSlice.ts
+++ client/src/redux/slices/userSlice.ts
@@ -3,17 +3,24 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 interface InitialState {
 	loggedIn: boolean
 	userID: string
+	sellerID: string
 }
 
-const initialState: InitialState = { loggedIn: false, userID: '' }
+interface Ids {
+	userID: string
+	sellerID: string
+}
+
+const initialState: InitialState = { loggedIn: false, userID: '', sellerID: '' }
 
 export const userSlice = createSlice({
 	name: 'user',
 	initialState,
 	reducers: {
-		login: (state, { payload }: PayloadAction<string>) => {
+		login: (state, { payload: { userID, sellerID } }: PayloadAction<Ids>) => {
 			state.loggedIn = true
-			state.userID = payload
+			state.userID = userID
+			state.sellerID = sellerID
 		},
 		logout: () => initialState,
 	},
