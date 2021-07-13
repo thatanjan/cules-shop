b8c5b264a51dcc38c60acf0f6503ecd887d37015
thatanjan <anjan.cules@gmail.com>
2021-07-13 15:55:10 +0600

    key property will be either string or swr key

--- client/src/graphql/createRequest.ts
+++ client/src/graphql/createRequest.ts
@@ -1,12 +1,13 @@
+import { Key } from 'swr'
 import graphQLClient from 'graphql/graphqlClient'
 
-interface Input<T> {
-	key: string
-	values: T | undefined
+export interface Input<K> {
+	key: string | Key
+	values: K | undefined
 }
 
 const createRequest = async <I, O>({ key, values }: Input<I>) => {
-	const data = await graphQLClient().request(key, values)
+	const data = await graphQLClient().request(key as string, values)
 	return data as O
 }
 
