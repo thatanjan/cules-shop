89e73d81299aa62f24d259c38072b8baea839061
thatanjan <anjan.cules@gmail.com>
2021-07-14 09:02:37 +0600

    getMyAddress query resolver added

--- server/src/graphql/Profile/profile.js
+++ server/src/graphql/Profile/profile.js
@@ -1,9 +1,10 @@
 import merge from 'lodash/merge'
 
 import getMultipleUserNameImage from './getMultipleUserNameImage.query'
+import getMyAddress from './getMyAddress.query'
 
 import typeDefs from './profile.types'
 
 export const profileTypeDefs = [typeDefs]
 
-export const profileResolvers = merge(getMultipleUserNameImage)
+export const profileResolvers = merge(getMultipleUserNameImage, getMyAddress)
