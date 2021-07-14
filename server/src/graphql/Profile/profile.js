2e8ba516f60e3b6116bb806215bd7f65c0f385b7
thatanjan <anjan.cules@gmail.com>
2021-07-14 09:22:01 +0600

    updateMyAddress mutation resolver added

--- server/src/graphql/Profile/profile.js
+++ server/src/graphql/Profile/profile.js
@@ -3,8 +3,14 @@ import merge from 'lodash/merge'
 import getMultipleUserNameImage from './getMultipleUserNameImage.query'
 import getMyAddress from './getMyAddress.query'
 
+import updateMyAddress from './updateMyAddress.mutation'
+
 import typeDefs from './profile.types'
 
 export const profileTypeDefs = [typeDefs]
 
-export const profileResolvers = merge(getMultipleUserNameImage, getMyAddress)
+export const profileResolvers = merge(
+	getMultipleUserNameImage,
+	getMyAddress,
+	updateMyAddress
+)
