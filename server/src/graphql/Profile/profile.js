e077c335d68f302e48c2093fff5c0428d49ad8ee
thatanjan <anjan.cules@gmail.com>
2021-07-14 14:12:12 +0600

    profile picture mutations are added to the main resolvers

--- server/src/graphql/Profile/profile.js
+++ server/src/graphql/Profile/profile.js
@@ -4,6 +4,7 @@ import getMultipleUserNameImage from './getMultipleUserNameImage.query'
 import getMyAddress from './getMyAddress.query'
 
 import updateMyAddress from './updateMyAddress.mutation'
+import profilePictureMutation from './profilePicture.mutation'
 
 import typeDefs from './profile.types'
 
@@ -12,5 +13,6 @@ export const profileTypeDefs = [typeDefs]
 export const profileResolvers = merge(
 	getMultipleUserNameImage,
 	getMyAddress,
-	updateMyAddress
+	updateMyAddress,
+	profilePictureMutation
 )
