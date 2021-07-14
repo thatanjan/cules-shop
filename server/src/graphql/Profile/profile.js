783f26954f6372368c0318e3781e665531c87202
thatanjan <anjan.cules@gmail.com>
2021-07-14 06:25:46 +0600

    getMultipleUserNameImage resolver added to the profile resolvers

--- server/src/graphql/Profile/profile.js
+++ server/src/graphql/Profile/profile.js
@@ -1,7 +1,9 @@
 import merge from 'lodash/merge'
 
+import getMultipleUserNameImage from './getMultipleUserNameImage.query'
+
 import typeDefs from './profile.types'
 
 export const profileTypeDefs = [typeDefs]
 
-export const profileResolvers = merge()
+export const profileResolvers = merge(getMultipleUserNameImage)
