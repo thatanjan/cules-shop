0a322899a90da5d32aca036c8883c6d3c0dc005f
thatanjan <anjan.cules@gmail.com>
2021-07-15 11:20:16 +0600

    all uploading state values are now nested inside an upload object. to make it more descriptive

--- client/src/redux/slices/profileSlice.ts
+++ client/src/redux/slices/profileSlice.ts
@@ -9,39 +9,45 @@ import { ProfilePictureUploadResponse } from 'interfaces/profile'
 import { Base64 } from 'interfaces/global'
 
 export interface InitialState {
-	previewLink: string
-	previewModal: boolean
-	file: Base64
-	uploading: boolean
-	successful: boolean
-	failed: boolean
-	alertProps: AlertProps | {}
+	upload: {
+		previewLink: string
+		previewModal: boolean
+		file: Base64
+		uploading: boolean
+		successful: boolean
+		failed: boolean
+		alertProps: AlertProps | {}
+	}
 }
 
 const initialState: InitialState = {
-	previewLink: '',
-	previewModal: false,
-	file: null,
-	uploading: false,
-	successful: false,
-	failed: false,
-	alertProps: {
-		severity: 'info',
-		message: '',
-		checked: false,
+	upload: {
+		previewLink: '',
+		previewModal: false,
+		file: null,
+		uploading: false,
+		successful: false,
+		failed: false,
+		alertProps: {
+			severity: 'info',
+			message: '',
+			checked: false,
+		},
 	},
 }
 
 export const uploadFile = createAsyncThunk(
 	'profilePictureUpload/uploadFileStatus',
 	async (_, { getState }) => {
-		const state = getState() as RootState
-
-		const image = state.profile.file
+		const {
+			profile: {
+				upload: { file },
+			},
+		} = getState() as RootState
 
 		return createRequest<{ image: Base64 }, ProfilePictureUploadResponse>({
 			key: uploadProfilePicture,
-			values: { image },
+			values: { image: file },
 		})
 	}
 )
@@ -51,42 +57,42 @@ const profilePictureUploadSlice = createSlice({
 	initialState,
 	reducers: {
 		openPreviewModal: (
-			state,
+			{ upload },
 			{ payload: previewLink }: PayloadAction<string>
 		) => {
-			state.previewLink = previewLink
-			state.previewModal = true
+			upload.previewLink = previewLink
+			upload.previewModal = true
 		},
-		closePreviewModal: state => {
-			state.previewLink = ''
-			state.previewModal = false
-			state.file = null
+		closePreviewModal: ({ upload }) => {
+			upload.previewLink = ''
+			upload.previewModal = false
+			upload.file = null
 		},
-		makeBase64Image: (state, { payload: file }: PayloadAction<Base64>) => {
-			state.file = file
+		makeBase64Image: ({ upload }, { payload: file }: PayloadAction<Base64>) => {
+			upload.file = file
 		},
 		resetState: () => initialState,
 	},
 	extraReducers: builder => {
 		builder
-			.addCase(uploadFile.pending, state => {
-				state.uploading = true
+			.addCase(uploadFile.pending, ({ upload }) => {
+				upload.uploading = true
 
-				state.alertProps = {
+				upload.alertProps = {
 					severity: 'info',
 					message: 'Profile picture is uploading',
 					checked: true,
 				}
 			})
 
-			.addCase(uploadFile.fulfilled, (state, { payload }) => {
-				state.uploading = false
-				state.successful = true
+			.addCase(uploadFile.fulfilled, ({ upload }, { payload }) => {
+				upload.uploading = false
+				upload.successful = true
 
 				const data = payload.uploadProfilePicture
 
 				if (data.success) {
-					state.alertProps = {
+					upload.alertProps = {
 						severity: 'success',
 						message: data.message,
 						checked: true,
@@ -94,18 +100,18 @@ const profilePictureUploadSlice = createSlice({
 				}
 
 				if (data.errorMessage) {
-					state.alertProps = {
+					upload.alertProps = {
 						severity: 'error',
 						message: data.errorMessage,
 						checked: true,
 					}
 				}
 			})
-			.addCase(uploadFile.rejected, state => {
-				state.uploading = false
-				state.failed = true
+			.addCase(uploadFile.rejected, ({ upload }) => {
+				upload.uploading = false
+				upload.failed = true
 
-				state.alertProps = {
+				upload.alertProps = {
 					severity: 'error',
 					message: 'Something went wrong. Please try again',
 					checked: true,
