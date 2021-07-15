0893f46eb6d996ec566f07eb273408879dc956fd
thatanjan <anjan.cules@gmail.com>
2021-07-15 19:30:45 +0600

    GetMultipleUserNameImage  interface is created

--- client/src/interfaces/profile.ts
+++ client/src/interfaces/profile.ts
@@ -3,3 +3,10 @@ import { CommonResponse } from 'interfaces/global'
 export interface ProfilePictureUploadResponse {
 	uploadProfilePicture: CommonResponse
 }
+
+export interface GetMultipleUserNameImage {
+	getMultipleUserNameImage: Array<{
+		name: string
+		image: string
+	}>
+}
