28a2d63e3deec5ed8e9f864d669f398cf66eaeae
thatanjan <anjan.cules@gmail.com>
2021-07-15 20:00:09 +0600

    logged in check added to the account index page

--- client/src/pages/account/index.tsx
+++ client/src/pages/account/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import jwtDecode from 'jwt-decode'
+import { GetServerSideProps } from 'next'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import Box from '@material-ui/core/Box'
+
+import checkValidJWT from 'utils/auth/checkValidJWT'
+
+import { UserPayload } from 'interfaces/authentication'
 
 import AccountData from 'components/Account/AccountData'
 import AccountAvatar from 'components/Avatar/AccountAvatar'
@@ -69,3 +75,23 @@ const AccountPage = (props: Props) => {
 }
 
 export default AccountPage
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+	const {
+		cookies: { jwt },
+	} = req
+
+	let props = { userID: '', sellerID: '' }
+
+	if (!jwt) return { props }
+
+	const isValid = await checkValidJWT(jwt)
+
+	const { userID, sellerID } = jwtDecode<UserPayload>(jwt)
+
+	if (!isValid) return { props }
+
+	props = { userID, sellerID: sellerID || '' }
+
+	return { props }
+}
