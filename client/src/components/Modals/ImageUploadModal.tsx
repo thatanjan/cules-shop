951b1e73f6412de9099e95ceee42a3d30cdf43d2
thatanjan <anjan.cules@gmail.com>
2021-07-15 13:58:31 +0600

    a component to drag and drop image to upload

---
+++ client/src/components/Modals/ImageUploadModal.tsx
@@ -0,0 +1,87 @@
+import { useDropzone } from 'react-dropzone'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import MuiDialogContent from '@material-ui/core/DialogContent'
+import DialogActions from '@material-ui/core/DialogActions'
+import PublishIcon from '@material-ui/icons/Publish'
+import Dialog from '@material-ui/core/Dialog'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+
+import makeBase64 from 'utils/makeBase64'
+
+import { Base64 } from 'interfaces/global'
+
+export type NullOrBooleanType = boolean | null
+
+const useStyles = makeStyles({
+	dialogContentStyle: {
+		display: 'grid',
+		height: '30vh',
+		placeItems: 'center',
+	},
+	uploadIconStyle: {},
+})
+
+interface Props {
+	closeModal: () => void
+	uploadModal: boolean
+	makeImage: (base64: Base64) => {}
+	openPreviewModal: (link: string) => void
+	closeReset: () => void
+}
+
+const UploadModal = ({
+	closeModal,
+	uploadModal,
+	makeImage,
+	openPreviewModal,
+	closeReset,
+}: Props) => {
+	const { dialogContentStyle, uploadIconStyle } = useStyles()
+
+	const { getRootProps, getInputProps } = useDropzone({
+		accept: 'image/*, video/*',
+		maxFiles: 1,
+		onDrop: acceptedFiles => {
+			const realFile = acceptedFiles[0]
+
+			const fileWithPreviewLink = Object.assign(realFile, {
+				previewLink: URL.createObjectURL(realFile),
+				valid: true,
+			})
+
+			openPreviewModal(fileWithPreviewLink.previewLink)
+
+			makeBase64(fileWithPreviewLink, makeImage)
+
+			closeModal()
+		},
+	})
+
+	return (
+		<>
+			<Dialog
+				onClose={closeModal}
+				aria-labelledby='simple-dialog-title'
+				open={uploadModal}
+				maxWidth='sm'
+				fullWidth
+			>
+				<DialogTitle style={{ textAlign: 'center' }} id='simple-dialog-title'>
+					Upload your Image
+				</DialogTitle>
+				<MuiDialogContent {...getRootProps()} className={dialogContentStyle}>
+					<input {...getInputProps()} />
+					<PublishIcon className={uploadIconStyle} fontSize='large' />
+				</MuiDialogContent>
+				<DialogActions>
+					<Button autoFocus onClick={closeReset} color='primary'>
+						Close
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</>
+	)
+}
+
+export default UploadModal
