8ccf84a90294eea92d0b47233f4a7e28bd7ba2d6
thatanjan <anjan.cules@gmail.com>
2021-07-16 14:25:25 +0600

    account avatar and name added

--- client/src/pages/account/become-seller.tsx
+++ client/src/pages/account/become-seller.tsx
@@ -6,6 +6,8 @@ import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import Box from '@material-ui/core/Box'
 
+import AccountAvatar from 'components/Avatar/AccountAvatar'
+
 import { UserPayload } from 'interfaces/authentication'
 
 import checkValidJWT from 'utils/auth/checkValidJWT'
@@ -15,7 +17,25 @@ interface Props {}
 const BecomeSeller = (props: Props) => {
 	return (
 		<>
-			<Typography>Become a Seller</Typography>
+			<Grid
+				container
+				alignItems='center'
+				sx={{
+					justifyContent: { xs: 'center', sm: 'start' },
+					maxWidth: '50rem',
+					margin: '3rem 0',
+				}}
+			>
+				<Grid item xs={6} sm={4}>
+					<AccountAvatar />
+				</Grid>
+
+				<Grid item xs={12} sm={6} sx={{ marginTop: { xs: '1rem', sm: 0 } }}>
+					<Typography variant='h2' component='h1' align='center'>
+						Taylor swift
+					</Typography>
+				</Grid>
+			</Grid>
 		</>
 	)
 }
