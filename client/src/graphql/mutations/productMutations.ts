3a9994eaf941d6ba5dc7ff2ebfee80b423db137c
thatanjan <anjan.cules@gmail.com>
2021-07-17 17:23:48 +0600

    price input added to the createProduct key

--- client/src/graphql/mutations/productMutations.ts
+++ client/src/graphql/mutations/productMutations.ts
@@ -6,6 +6,7 @@ export const createProduct = gql`
 		$shortDescription: String!
 		$description: String!
 		$quantity: Int!
+		$price: Int!
 		$category: ID!
 	) {
 		createProduct(
@@ -15,6 +16,7 @@ export const createProduct = gql`
 				description: $description
 				quantity: $quantity
 				category: $category
+				price: $price
 			}
 		) {
 			success
