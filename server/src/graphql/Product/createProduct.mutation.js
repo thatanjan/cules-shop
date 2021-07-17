e22b56113b7a84c94b324bb836a9c13f73678e52
thatanjan <anjan.cules@gmail.com>
2021-07-17 18:40:30 +0600

    product image upload functionality added from server side

--- server/src/graphql/Product/createProduct.mutation.js
+++ server/src/graphql/Product/createProduct.mutation.js
@@ -1,11 +1,26 @@
 import sendErrorMessage from 'utils/errorMessage'
+import uploadImage from 'utils/cloudinary/uploadToCloudinary'
 import Product from 'models/Product'
+import imageConfig from 'variables/cloudinaryVariables'
+
+const PATH = 'cules-shop/products/'
 
 const resolver = {
 	Mutation: {
 		createProduct: async (_, { Input }, { user: { sellerID } }) => {
 			try {
+				const { image } = Input
+
 				const product = new Product({ ...Input, seller: sellerID })
+
+				const imageID = await uploadImage(image, {
+					folder: PATH,
+					...imageConfig,
+				})
+
+				if (typeof imageID === 'string') {
+					product.image = imageID
+				}
 
 				await product.save()
 
