ed60c76cab9aa228dc3fe3ea87516fa1e9d29ae2
thatanjan <anjan.cules@gmail.com>
2021-07-17 14:17:09 +0600

    custom swr hook for getting all category names

---
+++ client/src/hooks/swr/useProductHooks.ts
@@ -0,0 +1,8 @@
+import useSWRgql from 'hooks/swr/useSWRgql'
+
+import { getAllCategoryNames } from 'graphql/queries/productQueries'
+
+import { AllCategoryName } from 'interfaces/product'
+
+export const useGetAllCategoryNames = () =>
+	useSWRgql<{}, AllCategoryName>({ key: getAllCategoryNames, values: {} })
