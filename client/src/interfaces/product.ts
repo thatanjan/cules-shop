a73506cba500fa1a296756b2c879dbde4dfa0784
thatanjan <anjan.cules@gmail.com>
2021-07-18 14:52:44 +0600

    GetProductDetailsResponse is created for query

--- client/src/interfaces/product.ts
+++ client/src/interfaces/product.ts
@@ -2,3 +2,13 @@ export type AllCategoryName = Array<{
 	name: string
 	categoryID: string
 }>
+
+export interface GetProductDetailsResponse {
+	name: string
+	description: string
+	category: string
+	seller: string
+	price: number
+	quantity: number
+	image: string
+}
