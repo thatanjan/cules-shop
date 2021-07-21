8f458d7c69e59db011740c1f97508e9b2a14c6dc
thatanjan <anjan.cules@gmail.com>
2021-07-21 09:28:39 +0600

    Category model will have it's own connection method

--- server/src/models/Category.js
+++ server/src/models/Category.js
@@ -1,10 +1,25 @@
-import { Schema, model } from 'mongoose'
+import { Schema, model, createConnection } from 'mongoose'
+import { config } from 'dotenv'
 
-import { connection } from './Product'
+config()
 
 const schema = new Schema({
 	name: { type: String, required: true },
 })
+
+export const connection = createConnection(
+	process.env.MONGO_URI_PRODUCT,
+	mongooseOptions
+)
+
+const mongooseOptions = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	bufferCommands: false,
+	bufferMaxEntries: 0,
+	useFindAndModify: false,
+	useCreateIndex: true,
+}
 
 const Category = connection.model('category', schema)
 
