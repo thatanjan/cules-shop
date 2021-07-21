0b394fc13adbaa7475356c2afa51ec61b7cb9fc3
thatanjan <anjan.cules@gmail.com>
2021-07-21 17:25:55 +0600

    GetAllCartProductsResponse interface is created for query

--- client/src/interfaces/cart.ts
+++ client/src/interfaces/cart.ts
@@ -2,3 +2,24 @@ export interface TotalCartItemsResponse {
 	totalItems: number
 	errorMessage: string
 }
+
+export interface CartProduct {
+	name: string
+	image: string
+	price: number
+	userQuantity: number
+	_id: string
+	seller: {
+		name: string
+		_id: string
+	}
+	category: {
+		name: string
+		_id: string
+	}
+}
+
+export interface GetAllCartProductsResponse {
+	cartProducts: Array<CartProduct>
+	errorMessage?: string
+}
