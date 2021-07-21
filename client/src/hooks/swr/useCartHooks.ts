abd29118f19f86769f5657741b25db9051483488
thatanjan <anjan.cules@gmail.com>
2021-07-21 17:26:19 +0600

    useGetAllCartProducts hook is created for query

--- client/src/hooks/swr/useCartHooks.ts
+++ client/src/hooks/swr/useCartHooks.ts
@@ -1,11 +1,20 @@
 import useSWRgql from 'hooks/swr/useSWRgql'
 
-import { totalCartItems } from 'graphql/queries/cartQueries'
+import { totalCartItems, getAllCartProducts } from 'graphql/queries/cartQueries'
 
-import { TotalCartItemsResponse } from 'interfaces/cart'
+import {
+	TotalCartItemsResponse,
+	GetAllCartProductsResponse,
+} from 'interfaces/cart'
 
 export const useTotalCartItems = () =>
 	useSWRgql<{}, { totalCartItems: TotalCartItemsResponse }>({
 		key: totalCartItems,
+		values: {},
+	})
+
+export const useGetAllCartProducts = () =>
+	useSWRgql<{}, { getAllCartProducts: GetAllCartProductsResponse }>({
+		key: getAllCartProducts,
 		values: {},
 	})
