54b4bf11b280c7868ff290c66fd28bc40fbe7ecf
thatanjan <anjan.cules@gmail.com>
2021-07-21 17:38:15 +0600

    all cart item data is fetched. if data is not ready custom backdrop will be shown

--- client/src/components/Cart/CartItemsShow.tsx
+++ client/src/components/Cart/CartItemsShow.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
+import { nanoid } from 'nanoid'
 
 import ProductPreview from 'components/Products/ProductPreview'
-
 import Pagination from 'components/Paginations/Pagination'
+import CustomBackdrop from 'components/Loaders/CustomBackdrop'
+
+import { useGetAllCartProducts } from 'hooks/swr/useCartHooks'
 
 interface Props {}
 
 const CartItemsShow = (props: Props) => {
+	const { data } = useGetAllCartProducts()
+
+	if (!data) return <CustomBackdrop />
+
 	return (
 		<>
 			<Grid container>
@@ -15,6 +22,7 @@ const CartItemsShow = (props: Props) => {
 					.fill(0)
 					.map(() => (
 						<Grid
+							key={nanoid()}
 							item
 							xs={6}
 							sm={4}
