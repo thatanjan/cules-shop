49a811786259cc8f02f384b154700028be6d0be2
thatanjan <anjan.cules@gmail.com>
2021-07-22 07:30:27 +0600

    getProductDetails query will also send category data with response based on the types

--- server/src/graphql/Product/getProductDetails.query.js
+++ server/src/graphql/Product/getProductDetails.query.js
@@ -3,7 +3,7 @@ import Product from 'models/Product'
 const resolver = {
 	Query: {
 		getProductDetails: async (_, { productID }) => {
-			const productDetails = await Product.findById(productID)
+			const productDetails = await Product.findById(productID).populate('category')
 			return productDetails
 		},
 	},
