89ee66d6c57b9aa1ec752d8c71feae337f64d8e3
thatanjan <anjan.cules@gmail.com>
2021-07-22 08:50:00 +0600

    after successful response getAllCartProducts query was not mutating now fix

--- client/src/components/Cart/DeleteFromCart.tsx
+++ client/src/components/Cart/DeleteFromCart.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { mutate } from 'swr'
 import Button from '@material-ui/core/Button'
 
-import { totalCartItems, getAllCartProducts } from 'graphql/queries/cartQueries'
+import { getAllCartProducts } from 'graphql/queries/cartQueries'
 import createRequest from 'graphql/createRequest'
 
 import { removeProductFromCart } from 'graphql/mutations/productMutations'
@@ -24,7 +24,7 @@ const DeleteFromCart = ({ productID }: Props) => {
 				{ removeProductFromCart: CommonResponse }
 			>({ key: removeProductFromCart, values: { productID } })
 
-			if (success) mutate(getAllCartProducts)
+			if (success) mutate([getAllCartProducts, undefined])
 		} catch (error) {}
 	}
 
