67f2240b251b2ce90a603c11c80122f33b9d9652
thatanjan <anjan.cules@gmail.com>
2021-07-23 11:41:01 +0600

    all cart item data will be fetched inside cart index file

--- client/src/pages/cart/index.tsx
+++ client/src/pages/cart/index.tsx
@@ -6,6 +6,9 @@ import Button from '@material-ui/core/Button'
 import CartItemsShow from 'components/Cart/CartItemsShow'
 import CartTotal from 'components/Cart/CartTotal'
 import CoupnInput from 'components/Cart/CoupnInput'
+import CustomBackdrop from 'components/Loaders/CustomBackdrop'
+
+import { useGetAllCartProducts } from 'hooks/swr/useCartHooks'
 
 interface Props {}
 
@@ -29,36 +32,48 @@ const CustomButton = ({ children }: CustomButtonProps) => {
 }
 
 const CartPage = (props: Props) => {
+	const { data } = useGetAllCartProducts()
+
+	if (!data) return <CustomBackdrop />
+
+	const {
+		getAllCartProducts: { cartProducts },
+	} = data
+
 	return (
 		<div>
 			<Typography component='h1' variant='h3' align='center'>
 				Cart
 			</Typography>
 
-			<CartItemsShow />
+			{cartProducts.length > 0 && (
+				<>
+					<CartItemsShow />
 
-			<Grid container sx={{ marginTop: '1rem' }}>
-				<Grid item xs={12} md={8} lg={6} sx={{ maxWidth: '50rem' }}>
-					<CoupnInput />
-				</Grid>
-				<Grid
-					item
-					container
-					xs={12}
-					lg={6}
-					justifyContent='flex-end'
-					alignItems='center'
-				>
-					<CustomButton>Update Cart</CustomButton>
-					<CustomButton>Procced to checkout</CustomButton>
-				</Grid>
-			</Grid>
+					<Grid container sx={{ marginTop: '1rem' }}>
+						<Grid item xs={12} md={8} lg={6} sx={{ maxWidth: '50rem' }}>
+							<CoupnInput />
+						</Grid>
+						<Grid
+							item
+							container
+							xs={12}
+							lg={6}
+							justifyContent='flex-end'
+							alignItems='center'
+						>
+							<CustomButton>Update Cart</CustomButton>
+							<CustomButton>Procced to checkout</CustomButton>
+						</Grid>
+					</Grid>
 
-			<Grid container justifyContent='flex-end'>
-				<Grid item xs={12} md={6}>
-					<CartTotal />
-				</Grid>
-			</Grid>
+					<Grid container justifyContent='flex-end'>
+						<Grid item xs={12} md={6}>
+							<CartTotal />
+						</Grid>
+					</Grid>
+				</>
+			)}
 		</div>
 	)
 }
