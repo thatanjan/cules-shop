dfdde308b0d5bb8f86fd2ef02a18394a6510413e
thatanjan <anjan.cules@gmail.com>
2021-07-23 12:21:44 +0600

    user's current address will be shown now on sepereate component with real data

--- client/src/pages/checkout/index.tsx
+++ client/src/pages/checkout/index.tsx
@@ -1,14 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Fragment } from 'react'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
 import Divider from '@material-ui/core/Divider'
+import { nanoid } from 'nanoid'
 
 import { useClearShippingAddress } from 'redux/hooks/useCheckoutHooks'
 
 import CheckoutForm from 'components/Forms/CheckoutForm'
 import CartTotal from 'components/Cart/CartTotal'
 import ShippingFormContainer from 'components/Forms/BillingForms/ShippingFormContainer'
+
+import { useGetMultipleProfile } from 'hooks/swr/useProfileHooks'
 
 interface Props {}
 
@@ -24,6 +27,36 @@ export const CheckoutPageTitle = ({ children }: CheckoutPageTitleProps) => (
 		<Divider />
 	</>
 )
+
+const AddressShow = () => {
+	const { data } = useGetMultipleProfile()
+
+	if (!data) return null
+
+	const {
+		getMultipleProfile: [{ address }],
+	} = data
+
+	const fields = Object.keys(address)
+
+	return (
+		<>
+			{fields.map(field => (
+				<Fragment key={nanoid()}>
+					<Grid item xs={4}>
+						{field}
+					</Grid>
+					<Grid item xs={2}>
+						:
+					</Grid>
+					<Grid item xs={6}>
+						{address[field]}
+					</Grid>
+				</Fragment>
+			))}
+		</>
+	)
+}
 
 const CheckoutPage = (props: Props) => {
 	const clearShippingAddress = useClearShippingAddress()
@@ -55,15 +88,7 @@ const CheckoutPage = (props: Props) => {
 							<Typography variant='h5'>Current Address</Typography>
 						</Grid>
 
-						<Grid item xs={4}>
-							name
-						</Grid>
-						<Grid item xs={2}>
-							:
-						</Grid>
-						<Grid item xs={6}>
-							Anjan Shomodder
-						</Grid>
+						<AddressShow />
 					</Grid>
 
 					<Grid item xs={12}>
