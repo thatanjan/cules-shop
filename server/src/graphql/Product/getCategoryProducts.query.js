217e8ff1f960ca73e68afa96db9787af1225b2a3
thatanjan <anjan.cules@gmail.com>
2021-07-25 09:36:11 +0600

    productids from cart has got with aggregation

--- server/src/graphql/Product/getCategoryProducts.query.js
+++ server/src/graphql/Product/getCategoryProducts.query.js
@@ -1,12 +1,38 @@
 import Product from 'models/Product'
+import Cart from 'models/Cart'
 import sendErrorMessage from 'utils/errorMessage'
+import convertObjectID from 'utils/convertObjectID'
 
 import { sortType } from 'variables/global'
+// $map: {
+// 								input: '$products.productID',
+// 								as: 'productID',
+// 								in: ['$$productID'],
+// 							},
 
 const resolver = {
 	Query: {
-		getCategoryProducts: async (_, { Input: { skip, categoryID, sortBy } }) => {
+		getCategoryProducts: async (
+			_,
+			{ Input: { skip, categoryID, sortBy } },
+			{ user: { userID } }
+		) => {
 			try {
+				const cartAggregation = Cart.aggregate()
+
+				const [
+					{
+						productIDs: [productIDs],
+					},
+				] = await cartAggregation
+					.match({
+						user: convertObjectID(userID),
+					})
+					.group({
+						_id: convertObjectID(userID),
+						productIDs: { $push: '$products.productID' },
+					})
+
 				const result = await Product.find(
 					{ category: categoryID },
 					'name quantity category price image'
@@ -18,6 +44,7 @@ const resolver = {
 
 				return { products: result }
 			} catch (e) {
+				console.log(e)
 				return sendErrorMessage()
 			}
 		},
