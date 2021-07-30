22cb7187afed4a6fce5e51288759664e45f651d5
thatanjan <anjan.cules@gmail.com>
2021-07-30 21:09:20 +0600

    authorization check added to search page

--- client/src/pages/search/[query].tsx
+++ client/src/pages/search/[query].tsx
@@ -1,5 +1,17 @@
 import React from 'react'
+import { GetServerSideProps } from 'next'
+import jwtDecode from 'jwt-decode'
 import Typography from '@material-ui/core/Typography'
+
+import { UserPayload } from 'interfaces/authentication'
+
+import checkValidJWT from 'utils/auth/checkValidJWT'
+
+interface Props {
+	userID: string
+	sellerID: string
+	query: string
+}
 
 const Query = () => {
 	return (
@@ -9,6 +21,29 @@ const Query = () => {
 			</Typography>
 		</>
 	)
+}
+
+export const getServerSideProps: GetServerSideProps = async ({
+	req,
+	query: { query },
+}) => {
+	const {
+		cookies: { jwt },
+	} = req
+
+	let props = { userID: '', sellerID: '', query }
+
+	if (!jwt) return { props }
+
+	const isValid = await checkValidJWT(jwt)
+
+	const { userID, sellerID } = jwtDecode<UserPayload>(jwt)
+
+	if (!isValid) return { props }
+
+	props = { ...props, userID, sellerID: sellerID || '' }
+
+	return { props }
 }
 
 export default Query
