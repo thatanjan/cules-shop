df580300dfa4c9f22e29e6bc722de1eea01212dd
thatanjan <anjan.cules@gmail.com>
2021-07-30 18:39:45 +0600

    search page is added

---
+++ client/src/pages/search/[query].tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+const Query = () => {
+	return (
+		<>
+			<Typography align='center' variant='h3' sx={{ m: '1rem 0' }}>
+				Search Products
+			</Typography>
+		</>
+	)
+}
+
+export default Query
