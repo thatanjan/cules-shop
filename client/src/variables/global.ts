b0683b7d4e45edaafe08c2604774771b7e6755ac
thatanjan <anjan.cules@gmail.com>
2021-07-31 08:46:44 +0600

    sortType object is created

--- client/src/variables/global.ts
+++ client/src/variables/global.ts
@@ -3,3 +3,9 @@ export const TOKEN_NAME = 'jwt'
 
 export const LOGIN_URL = 'authentication/login'
 export const REGISTER_URL = 'authentication/register'
+
+export const sortType = {
+	NAME: 'name',
+	HIGH_PRICE: '-price',
+	LOW_PRICE: 'price',
+}
