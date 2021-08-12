da86630ada51a21a03542c33c40e406a48d24e6e
thatanjan <anjan.cules@gmail.com>
2021-08-12 18:38:56 +0600

    Checkout mutation is fixed. now this query can accept array of object.

--- client/src/graphql/mutations/checkoutMutations.ts
+++ client/src/graphql/mutations/checkoutMutations.ts
@@ -2,26 +2,14 @@ import { gql } from 'graphql-request'
 
 export const checkout = gql`
 	mutation checkout(
-		$productID: ID!
-		$categoryID: ID!
-		$userQuantity: Int!
+		$products: [CheckoutProduct!]
+		$shippingDetails: ShippingInput!
 		$stripeID: ID!
 	) {
 		checkout(
 			Input: {
-				products: [
-					{
-						productID: $productID
-						categoryID: $categoryID
-						userQuantity: $userQuantity
-					}
-				]
-				shippingAddress: {
-					country: $country
-					city: $city
-					postal: $postal
-					address: $address
-				}
+				products: $products
+				shippingDetails: $shippingDetails
 				stripeID: $stripeID
 			}
 		) {
@@ -30,3 +18,40 @@ export const checkout = gql`
 		}
 	}
 `
+// export const checkout = gql`
+// 	mutation checkout(
+// 		$products: [CheckoutProduct!]
+// 		$productID: ID!
+// 		$categoryID: ID!
+// 		$userQuantity: Int!
+// 		$name: String!
+// 		$country: String!
+// 		$city: String!
+// 		$postal: String!
+// 		$address: String!
+// 		$stripeID: ID!
+// 	) {
+// 		checkout(
+// 			Input: {
+// 				products: [
+// 					{
+// 						productID: $productID
+// 						categoryID: $categoryID
+// 						userQuantity: $userQuantity
+// 					}
+// 				]
+// 				shippingDetails: {
+// 					country: $country
+// 					city: $city
+// 					postal: $postal
+// 					address: $address
+// 					name: $name
+// 				}
+// 				stripeID: $stripeID
+// 			}
+// 		) {
+// 			success
+// 			errorMessage
+// 		}
+// 	}
+// `
