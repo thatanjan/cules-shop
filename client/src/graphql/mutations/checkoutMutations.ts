8ca79eec192e17b367d52bb1cb705499f47b2097
thatanjan <anjan.cules@gmail.com>
2021-08-13 12:09:53 +0600

    no need to send products for checkout

--- client/src/graphql/mutations/checkoutMutations.ts
+++ client/src/graphql/mutations/checkoutMutations.ts
@@ -1,57 +1,10 @@
 import { gql } from 'graphql-request'
 
 export const checkout = gql`
-	mutation checkout(
-		$products: [CheckoutProduct!]
-		$shippingDetails: ShippingInput!
-		$stripeID: ID!
-	) {
-		checkout(
-			Input: {
-				products: $products
-				shippingDetails: $shippingDetails
-				stripeID: $stripeID
-			}
-		) {
+	mutation checkout($shippingDetails: ShippingInput!, $stripeID: ID!) {
+		checkout(Input: { shippingDetails: $shippingDetails, stripeID: $stripeID }) {
 			success
 			errorMessage
 		}
 	}
 `
-// export const checkout = gql`
-// 	mutation checkout(
-// 		$products: [CheckoutProduct!]
-// 		$productID: ID!
-// 		$categoryID: ID!
-// 		$userQuantity: Int!
-// 		$name: String!
-// 		$country: String!
-// 		$city: String!
-// 		$postal: String!
-// 		$address: String!
-// 		$stripeID: ID!
-// 	) {
-// 		checkout(
-// 			Input: {
-// 				products: [
-// 					{
-// 						productID: $productID
-// 						categoryID: $categoryID
-// 						userQuantity: $userQuantity
-// 					}
-// 				]
-// 				shippingDetails: {
-// 					country: $country
-// 					city: $city
-// 					postal: $postal
-// 					address: $address
-// 					name: $name
-// 				}
-// 				stripeID: $stripeID
-// 			}
-// 		) {
-// 			success
-// 			errorMessage
-// 		}
-// 	}
-// `
