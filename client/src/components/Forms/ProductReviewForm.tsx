83a965f5d788768e3e084828d554e6f26c432def
thatanjan <anjan.cules@gmail.com>
2021-08-15 13:03:41 +0600

    adding review is functionality is added

--- client/src/components/Forms/ProductReviewForm.tsx
+++ client/src/components/Forms/ProductReviewForm.tsx
@@ -1,15 +1,40 @@
 import React, { useState, FormEventHandler } from 'react'
+import { useRouter } from 'next/router'
 import Box from '@material-ui/core/Box'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 
 import CustomRating from 'components/Ratings/CustomRating'
 
+import createRequest from 'graphql/createRequest'
+
+import { AddReviewInput } from 'interfaces/product'
+import { CommonResponse } from 'interfaces/global'
+
+import { addReview } from 'graphql/mutations/productMutations'
+
 const ProductReviewForm = () => {
 	const [description, setDescription] = useState('')
 	const [ratingValue, setRatingValue] = useState(0)
 
-	const handleSubmit: FormEventHandler<HTMLFormElement> = event => {
+	const {
+		query: { productID },
+	} = useRouter()
+
+	const handleSubmit: FormEventHandler<HTMLFormElement> = async event => {
+		event.preventDefault()
+		const values: AddReviewInput = {
+			description,
+			star: ratingValue,
+			productID: productID as string,
+		}
+		const request = await createRequest<AddReviewInput, CommonResponse>({
+			key: addReview,
+			values: values,
+		})
+
+		console.log(request)
+
 		setDescription('')
 		setRatingValue(0)
 		event.preventDefault()
