18052228d955e6e74dd51f2e6459d034a9bf1ca9
thatanjan <anjan.cules@gmail.com>
2021-08-15 18:00:20 +0600

    instead of storing user id , Product review will now store user's profile id

--- server/src/graphql/Product/addReview.mutation.js
+++ server/src/graphql/Product/addReview.mutation.js
@@ -1,5 +1,6 @@
 import sendErrorMessage from 'utils/errorMessage'
 import Product from 'models/Product'
+import Profile from 'models/Profile'
 
 const resolver = {
 	Mutation: {
@@ -14,10 +15,12 @@ const resolver = {
 				const ratingUpdateObject = {}
 				ratingUpdateObject[starKey] = 1
 
+				const { _id } = await Profile.findOne({ user: userID }, '_id')
+
 				const update = await Product.updateOne(
 					{ _id: productID },
 					{
-						$push: { reviews: { description, star, user: userID } },
+						$push: { reviews: { description, star, user: _id } },
 						$inc: ratingUpdateObject,
 					}
 				)
