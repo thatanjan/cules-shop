6a4814dfafd53b75ec1763cbce5d07b70760d490
thatanjan <anjan.cules@gmail.com>
2021-08-16 11:18:34 +0600

    original reviews are now fetched from server and data is passed to Review component

--- client/src/components/Reviews/ReviewList.tsx
+++ client/src/components/Reviews/ReviewList.tsx
@@ -1,22 +1,35 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import { nanoid } from 'nanoid'
+
+import { useGetReviews } from 'hooks/swr/useProductHooks'
 
 import Review from './Review'
 
 interface Props {}
 
 const ReviewList = (props: Props) => {
+	const {
+		query: { productID },
+	} = useRouter()
+	const { data, error } = useGetReviews(productID as string)
+
+	if (!data) return null
+
+	const {
+		getReviews: { reviews },
+	} = data
+
+	console.log(data)
 	return (
 		<List>
-			{Array(10)
-				.fill(0)
-				.map(() => (
-					<ListItem key={nanoid()}>
-						<Review />
-					</ListItem>
-				))}
+			{reviews.map(review => (
+				<ListItem key={nanoid()}>
+					<Review {...review} />
+				</ListItem>
+			))}
 		</List>
 	)
 }
