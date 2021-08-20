8a6ba6dbfd8c517b76966e137ebcefb91b1e84df
thatanjan <anjan.cules@gmail.com>
2021-08-20 11:38:44 +0600

    text and button added to the top of banner

--- client/src/components/Banner/BannerSlideShow.tsx
+++ client/src/components/Banner/BannerSlideShow.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { makeStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
 import { nanoid } from 'nanoid'
 
 const useStyles = makeStyles({
@@ -14,6 +17,9 @@ const useStyles = makeStyles({
 
 const banners = ['cules-shop/pixel_2_xl_google-actualizacion-seguridad_wscp9z']
 
+// autoplay={{
+// 	delay: 3000,
+// }}
 const BannerSlideShow = () => {
 	const { swiperContainer } = useStyles()
 	return (
@@ -21,20 +27,48 @@ const BannerSlideShow = () => {
 			<Swiper
 				centeredSlides
 				loop
-				autoplay={{
-					delay: 3000,
-				}}
 				pagination={{
 					clickable: true,
 				}}
 				navigation
 				className={swiperContainer}
 			>
+				{/* <SwiperSlide key={nanoid()}> */}
 				{banners.map(item => (
-					<SwiperSlide key={nanoid()}>
+					<Box sx={{ position: 'relative' }}>
+						<Box
+							sx={{
+								position: 'absolute',
+								zIndex: '9999',
+								left: '10%',
+								display: 'grid',
+								height: '100%',
+								gridRowGap: '2%',
+							}}
+						>
+							<Typography
+								variant='h4'
+								sx={{ textTransform: 'capitalize', alignSelf: 'end' }}
+							>
+								Explore the new Technologies
+							</Typography>
+							<Button
+								variant='outlined'
+								sx={{
+									alignSelf: 'start',
+									justifySelf: 'start',
+									padding: {
+										sm: '2% 20%',
+									},
+								}}
+							>
+								EXPLORE
+							</Button>
+						</Box>
 						<Image src={item} width={1920} height={1080} layout='responsive' />
-					</SwiperSlide>
+					</Box>
 				))}
+				{/* </SwiperSlide> */}
 			</Swiper>
 		</>
 	)
