a57786c169350e64780e066f9e85c46f02e0bfa7
thatanjan <anjan.cules@gmail.com>
2021-08-29 20:02:10 +0600

    getAllSellerProducts query resolver is created

--- server/src/graphql/Product/product.js
+++ server/src/graphql/Product/product.js
@@ -9,6 +9,7 @@ import getProductDetails from './getProductDetails.query'
 import getCategoryProducts from './getCategoryProducts.query'
 import searchProducts from './searchProducts.query'
 import getReviews from './getReviews.query'
+import getAllSellerProducts from './getAllSellerProducts.query'
 
 export const productTypeDefs = [typeDefs]
 
@@ -19,5 +20,6 @@ export const productResolvers = merge(
 	getCategoryProducts,
 	searchProducts,
 	addReview,
-	getReviews
+	getReviews,
+	getAllSellerProducts
 )
