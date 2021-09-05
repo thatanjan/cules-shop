76b0e03129992ccd772bde551702758c6aeb3ff2
thatanjan <anjan.cules@gmail.com>
2021-09-05 18:07:23 +0600

    3 more ProductPreviewTabs added for more categories

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -47,7 +47,16 @@ const Index = (props: Props) => {
 		<>
 			<ProductBannerSlideShow />
 			<ProductPreviewTabs
-				tabData={[new TabData('Television', '60f27c41389742613420479a')]}
+				tabData={[new TabData('Televisions', '60f27c41389742613420479a')]}
+			/>
+			<ProductPreviewTabs
+				tabData={[new TabData('Mobiles', '61345b7eda0b1e3e4df1ee9f')]}
+			/>
+			<ProductPreviewTabs
+				tabData={[new TabData('Air Conditioners', '61345bc0da0b1e3e4df1eea0')]}
+			/>
+			<ProductPreviewTabs
+				tabData={[new TabData('Laptops', '61345c95da0b1e3e4df1eea2')]}
 			/>
 		</>
 	)
