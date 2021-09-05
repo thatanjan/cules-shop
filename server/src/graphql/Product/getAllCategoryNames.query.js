50bb92818da228780358d5ac181a6f752aebfd71
thatanjan <anjan.cules@gmail.com>
2021-09-05 16:28:07 +0600

    GetAllCategoryNames will now return the modified type of GetAllCategoryNamesResponse. Also it will return errorMessage

--- server/src/graphql/Product/getAllCategoryNames.query.js
+++ server/src/graphql/Product/getAllCategoryNames.query.js
@@ -1,13 +1,16 @@
+import sendErrorMessage from 'utils/errorMessage'
 import Category from 'models/Category'
 
 const resolver = {
 	Query: {
 		getAllCategoryNames: async () => {
-			const names = await Category.find({}, 'name')
+			try {
+				const categories = await Category.find({})
 
-			const result = names.map(({ name, _id }) => ({ name, categoryID: _id }))
-
-			return result
+				return { categories }
+			} catch (e) {
+				return sendErrorMessage()
+			}
 		},
 	},
 }
