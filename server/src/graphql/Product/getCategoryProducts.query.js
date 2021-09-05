35b7a790e1dda7d1166dc4c7a52d100c36f588e3
thatanjan <anjan.cules@gmail.com>
2021-09-05 16:33:55 +0600

    getCategoryProducts query resolver will not use lookup to populate category. But it will use regular populate.

--- server/src/graphql/Product/getCategoryProducts.query.js
+++ server/src/graphql/Product/getCategoryProducts.query.js
@@ -53,22 +53,11 @@ const resolver = {
 					.sort(sortType[sortBy])
 					.skip(skip)
 					.limit(30)
-					.lookup({
-						from: 'categories',
-						localField: 'name _id',
-						foreignField: 'categoryID',
-						as: 'category',
-					})
-					.unwind('$category')
 					.project(projection)
 
-				const [{ totalProducts }] = await Product.aggregate()
-					.match({
-						category: convertObjectID(categoryID),
-					})
-					.count('totalProducts')
+				await Product.populate(products, 'category')
 
-				return { products, totalProducts }
+				return { products, totalProducts: products.length }
 			} catch (e) {
 				return sendErrorMessage()
 			}
