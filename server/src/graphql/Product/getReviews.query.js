21e3b4f42f3927b1e78292b42003d264ad1ca33c
thatanjan <thatanjan@gmail.com>
2021-10-09 17:26:29 +0600

    a try-catch block added to the getReviews resolver

--- server/src/graphql/Product/getReviews.query.js
+++ server/src/graphql/Product/getReviews.query.js
@@ -1,50 +1,56 @@
 import Product from 'models/Product'
 
+import sendErrorMessage from 'utils/errorMessage'
+
 const resolver = {
 	Query: {
 		getReviews: async (_, { productID }) => {
-			let product = await Product.findById(productID)
-				.sort('-reviews.date')
-				.populate({
-					path: 'reviews.user',
-					select: 'name profilePicture user',
+			try {
+				let product = await Product.findById(productID)
+					.sort('-reviews.date')
+					.populate({
+						path: 'reviews.user',
+						select: 'name profilePicture user',
+					})
+
+				product = product.toObject()
+
+				const { allStars, reviews } = product
+
+				let totalReviews = 0
+				let averageStars = 0
+
+				if (!reviews || reviews.length === 0)
+					return { reviews: [], totalReviews, averageStars }
+
+				const keys = Object.keys(allStars)
+
+				keys.forEach(key => {
+					const value = allStars[key]
+					const numKey = parseInt(key, 10)
+
+					totalReviews += value
+					averageStars += value * numKey
 				})
 
-			product = product.toObject()
+				averageStars /= totalReviews
+				averageStars = averageStars.toFixed(1) * 10
 
-			const { allStars, reviews } = product
+				reviews.forEach(review => {
+					// eslint-disable-next-line no-param-reassign
+					review.date = review.date.toDateString()
+				})
 
-			let totalReviews = 0
-			let averageStars = 0
+				const sortedReviews = []
 
-			if (!reviews || reviews.length === 0)
-				return { reviews: [], totalReviews, averageStars }
+				for (let i = reviews.length - 1; i >= 0; i--) {
+					sortedReviews.push(reviews[i])
+				}
 
-			const keys = Object.keys(allStars)
-
-			keys.forEach(key => {
-				const value = allStars[key]
-				const numKey = parseInt(key, 10)
-
-				totalReviews += value
-				averageStars += value * numKey
-			})
-
-			averageStars /= totalReviews
-			averageStars = averageStars.toFixed(1) * 10
-
-			reviews.forEach(review => {
-				// eslint-disable-next-line no-param-reassign
-				review.date = review.date.toDateString()
-			})
-
-			const sortedReviews = []
-
-			for (let i = reviews.length - 1; i >= 0; i--) {
-				sortedReviews.push(reviews[i])
+				return { reviews: sortedReviews, totalReviews, averageStars }
+			} catch (e) {
+				return sendErrorMessage()
 			}
-
-			return { reviews: sortedReviews, totalReviews, averageStars }
 		},
 	},
 }
