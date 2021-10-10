8ce2288f706f82e46c0586550cac4c29526e0cbe
thatanjan <thatanjan@gmail.com>
2021-10-10 18:23:21 +0600

    Product model will use the category db connection object

--- server/src/models/Product.js
+++ server/src/models/Product.js
@@ -1,12 +1,9 @@
-import { Schema, createConnection } from 'mongoose'
-import { config } from 'dotenv'
+import { Schema } from 'mongoose'
 
 import Profile from 'models/Profile'
 // eslint-disable-next-line import/no-cycle
 import Seller from 'models/Seller'
-import Category from 'models/Category'
-
-config()
+import Category, { connection } from 'models/Category'
 
 const objectId = Schema.Types.ObjectId
 
@@ -64,20 +61,6 @@ const schema = new Schema({
 		5: { type: Number, default: 0 },
 	},
 })
-
-const mongooseOptions = {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	bufferCommands: false,
-	bufferMaxEntries: 0,
-	useFindAndModify: false,
-	useCreateIndex: true,
-}
-
-export const connection = createConnection(
-	process.env.MONGO_URI_PRODUCT,
-	mongooseOptions
-)
 
 const product = connection.model('product', schema)
 
