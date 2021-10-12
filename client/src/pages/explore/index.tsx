06c47ea66bdfe3216c582bf6567054dc3c72d14a
thatanjan <thatanjan@gmail.com>
2021-10-12 11:30:07 +0600

    authentication added to explore page

--- client/src/pages/explore/index.tsx
+++ client/src/pages/explore/index.tsx
@@ -1,15 +1,35 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 
-import CustomBackdrop from 'components/Loaders/CustomBackdrop'
+import jwtDecode from 'jwt-decode'
+import { GetServerSideProps } from 'next'
+
+import { useStoreID } from 'redux/hooks/useUserHooks'
+
+import ProductBannerSlideShow from 'components/Banner/ProductBannerSlideShow'
+import ProductPreviewTabs from 'components/Tabs/ProductPreviewTabs'
 import CustomAlert from 'components/Alerts/CustomAlert'
+
+import CustomBackdrop from 'components/Loaders/CustomBackdrop'
+
+import { UserPayload } from 'interfaces/authentication'
+
+import checkValidJWT from 'utils/auth/checkValidJWT'
+
 import ProductsShow from 'components/Products/ProductsShow'
 
 import { useGetPopularProducts } from 'hooks/swr/useProductHooks'
 
 import { totalCartItems } from 'graphql/queries/cartQueries'
 
-const Explore = () => {
+interface Props {
+	userID: string
+	sellerID: string
+}
+
+const Explore = (props: Props) => {
+	useStoreID(props)
+
 	const { data, error, isValidating } = useGetPopularProducts()
 
 	if (error) return <CustomAlert checked severity='error' />
@@ -38,3 +58,23 @@ const Explore = () => {
 }
 
 export default Explore
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+	const {
+		cookies: { jwt },
+	} = req
+
+	let props = { userID: '', sellerID: '' }
+
+	if (!jwt) return { props }
+
+	const isValid = await checkValidJWT(jwt)
+
+	if (!isValid) return { props }
+
+	const { userID, sellerID } = jwtDecode<UserPayload>(jwt)
+
+	props = { userID, sellerID: sellerID || '' }
+
+	return { props }
+}
