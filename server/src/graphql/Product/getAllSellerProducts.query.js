9e003c29c38f314055c442ec6a0586d383ee7be0
thatanjan <thatanjan@gmail.com>
2021-10-20 17:09:06 +0600

    getAllSellerProducts query was giving unexpected results. now it is fixed.

--- server/src/graphql/Product/getAllSellerProducts.query.js
+++ server/src/graphql/Product/getAllSellerProducts.query.js
@@ -6,21 +6,19 @@ const resolver = {
 	Query: {
 		getAllSellerProducts: async (_, __, { user: { sellerID } }) => {
 			try {
-				const productAggregation = Product.aggregate()
 				const projection = { name: 1, quantity: 1, category: 1, price: 1, image: 1 }
-				const products = await productAggregation
-					.match({
-						seller: convertObjectID(sellerID),
-					})
-					.lookup({
-						from: 'categories',
-						localField: 'name _id',
-						foreignField: 'categoryID',
-						as: 'category',
-					})
-					.unwind('$category')
-					.project(projection)
 
+				const products = await Product.find(
+					{
+						seller: convertObjectID(sellerID),
+					},
+					projection
+				).populate({
+					path: 'category',
+					select: '_id name',
+				})
+
+				console.log(products)
 				return { products }
 			} catch (e) {
 				return sendErrorMessage()
